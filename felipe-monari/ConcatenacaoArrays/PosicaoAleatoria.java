d461f7572089cfdaed875320078ae08b9ec8426b
Monari14 <133614580+Monari14@users.noreply.github.com>
2024-02-07 00:26:36 +0000

    Leitura com arrays 06/02/2024

---
+++ felipe-monari/ConcatenacaoArrays/PosicaoAleatoria.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Random;
+
+class PosicaoAleatoria {
+    public static void main(String[] args) {
+        Random rnm = new Random();
+        String a = (Arrays.deepToString(args));
+        System.out.println(a);
+        System.out.println(a.charAt(rnm.nextInt(a.length()-1)));
+        }
+}
+
---
+++ felipe-monari/ConcatenacaoArrays/PosicaoEspecifica.java
@@ -0,0 +1,8 @@
+
+class PosicaoEspecifica {
+    public static void main(String[] args) {
+        Integer letraPos = Integer.parseInt(args[0]);
+        String a = args[1];
+        System.out.println(a.charAt(letraPos -1));
+        }
+}
